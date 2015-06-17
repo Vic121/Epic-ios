d1fc22c64a2b64c2847a131de642c20457d7df7f
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2015-06-17 07:49:43 +0200

    Removed classes without content

--- EpicGlue/ItemModelFactory.m
+++ EpicGlue/ItemModelFactory.m
@@ -8,38 +8,11 @@
 
 #import "ItemModelFactory.h"
 
-#import "ImageItemModel.h"
-#import "VideoItemModel.h"
-#import "TextItemModel.h"
-#import "TweetItemModel.h"
-#import "LinkItemModel.h"
-
 @implementation ItemModelFactory
 
 +(ItemModel *)modelFromJSON:(NSDictionary *)json;
 {
-    NSString *mediaType = [[json objectForKey:@"media_type"] string];
-    
-    if ([mediaType isEqualToString:@"image"])
-    {
-        return [ImageItemModel fromJSON:json];
-    }
-    else if ([mediaType isEqualToString:@"video"])
-    {
-        return [VideoItemModel fromJSON:json];
-    }
-    else if ([mediaType isEqualToString:@"text"])
-    {
-        return [TextItemModel fromJSON:json];
-    }
-    else if ([mediaType isEqualToString:@"tweet"])
-    {
-        return [TweetItemModel fromJSON:json];
-    }
-    else if ([mediaType isEqualToString:@"link"])
-    {
-        return [LinkItemModel fromJSON:json];
-    }
+    NSString *mediaType = [json[@"media_type"] string];
     
     NSLog(@"MediaType %@ not recognized", mediaType);
     return [ItemModel fromJSON:json];
